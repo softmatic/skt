5b34daa0f8fd587dfe4a3cfcb913f94d97d1b586
Softmatic GmbH <info@softmatic.com>
2025-01-03 12:18:36 +0100

    Update +page.svelte

--- src/routes/(public)/+page.svelte
+++ src/routes/(public)/+page.svelte
@@ -5,6 +5,13 @@
 <div class="grow flex flex-col bg-gray-50 dark:bg-gray-900">
 	<div class="m-auto text-center space-y-4">
 		<h1>Welcome</h1>
-		<p>Welcome message</p>
+		<p>Welcome to SKT, the Sveltekit Boilerplate</p>
+		<p>
+			For an introduction and sources, visit our <a
+				aria-label="SKT Github Repo"
+				class="link"
+				href="https://github.com/softmatic/skt">Github Repo</a
+			>
+		</p>
 	</div>
 </div>
